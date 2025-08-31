b70a0179c0bf2dcb464df0f92d3dba0401f9266a
Rafael Callegari De Oliveira <137407464+CALLEGARii@users.noreply.github.com>
2025-08-31 16:03:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,21 @@ const app = express();
 const port = 3000;
 const db = require("./config/db");
 
-// Configuração do EJS
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// rotas API
 const salasRoutes = require("./routes/SalasDeAulaRoutes");
 app.use("/api/salas", salasRoutes);
 
-// rota HTML para exibir as salas
 const SalasDeAula = require("./models/SalasDeAula");
 app.get("/salas", async (req, res) => {
   const salas = await SalasDeAula.findAll({ where: { removido: false } });
   res.render("salas", { salas });
 });
 
-// conecta ao banco
 db.sync().then(() => {
   console.log("Banco sincronizado!");
   app.listen(port, () => {
